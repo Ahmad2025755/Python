bc1231725bb682048af697d3f5f8c57f019b33d0
Ahmad2025755 <ahmad.alain2022@gmail.com>
2025-08-29 16:06:51 +0500

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,23 @@
+num1 = 45
+num2 = 3
+print("Addition:", num1+num2)
+print("Difference:", num1-num2)
+
+
+print("Floor Division:",num1//num2)
+print("Mudulus Operator:", num1%num2)
+print("Square:", num1**num2)
+print("Square root:", num1**0.5)
+print()
+
+
+print(num1==num2)
+print(num1>num2)
+print(num1<num2)
+print(num1!=num2)
+
+
+print()   #EBODMAS
+
+result= num1/2+num2**2+10   #22.5 + 19 = 41.5
+print("Result is", result)
