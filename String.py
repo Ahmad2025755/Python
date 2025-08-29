239350cbe3c85777f2e23b75d386cf676b44669b
Ahmad2025755 <ahmad.alain2022@gmail.com>
2025-08-29 16:16:07 +0500

    Add files via upload

---
+++ String.py
@@ -0,0 +1,12 @@
+first_name = "Codingal"
+last_name = "Education"
+full_name = first_name+last_name
+
+
+print("Full Name is", full_name)
+
+
+word = 'Coding'
+print("Length", len(word))
+print("first letter:", word[0])
+print("Sliced:", word [0:3])
