a37de490009f9341e645c8ad8b1e0055dc9716b1
Ahmad2025755 <ahmad.alain2022@gmail.com>
2025-09-26 16:16:21 +0500

    Add files via upload

---
+++ AbsoluteValue.py
@@ -0,0 +1,11 @@
+def Absolute_Value(number):
+    if number >=0:
+
+        return number
+    
+    else:
+        return -number
+    
+
+print("Absolute value of the -72:", Absolute_Value(89))
+print("Absolute value of the 89:" , Absolute_Value(-89))
