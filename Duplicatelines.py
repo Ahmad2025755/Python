2cd3540aacc02858b2f553b416da8c46b605809d
Ahmad2025755 <ahmad.alain2022@gmail.com>
2025-10-29 17:23:34 +0500

    Add files via upload

---
+++ Duplicatelines.py
@@ -0,0 +1,23 @@
+inputFile = open('repeated.txt','r')
+
+outputFile = open('updatedfile.txt','w')
+
+
+lines_seen_so_far = set()
+print("Eliminating duplicate lines....")
+
+for line in inputFile:
+
+
+    if line not in lines_seen_so_far:
+
+
+        outputFile.write(line)
+
+
+        lines_seen_so_far.add(line)
+
+
+inputFile.close()
+outputFile.close()
+      
