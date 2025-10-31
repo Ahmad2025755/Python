9bdcb917fede97a8d8d16f33119dd234c54bd26e
Ahmad2025755 <ahmad.alain2022@gmail.com>
2025-10-31 16:12:00 +0500

    Add files via upload

---
+++ Guess.py
@@ -0,0 +1,77 @@
+import random
+
+lower_bound = 1
+upper_bound = 7
+max_attempts = 5
+
+secret_number = random.randint(lower_bound, upper_bound)
+
+
+
+def get_guess():
+
+    while True:
+        
+        guess = int(input("Guess a number between 1 and 7:"))
+
+
+        if lower_bound <= guess <= upper_bound:
+
+            return guess
+
+        else:
+            print("Invalid number. Please enter a number between the specified range.")
+
+
+
+def check_guess(guess, secret_number):
+
+    if guess == secret_number:
+        return("Correct")
+
+    elif guess < secret_number:
+
+        return "Too low"
+
+    else:
+        
+        return "Too high"
+
+
+
+
+def play_game():
+
+    attempts = 0
+    won = False
+
+
+    while attempts < max_attempts:
+
+
+        attemps = attempts + 1
+        guess = get_guess()
+        result = check_guess(guess, secret_number)
+
+
+
+        if result == "Correct":
+
+            print(f"Congragulations! You've guessed the correct number, {secret_number}, in {attempts} attempts.")
+            won = True
+            break
+
+        else:
+            print(f"{result}. Please try again.")
+
+
+    if not won:
+        print(f"Sorry! you ran out of attempts, the secret number is {secret_number}.")
+
+
+
+if __name__ == "__main__":
+    print("Welcome to the number guessing game!")
+    play_game()
+
+
